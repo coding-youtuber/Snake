c7de53af8417058cc5b161af65499bf2bfdb12ef
直也テック <58985013+coding-youtuber@users.noreply.github.com>
2020-11-06 21:29:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,125 +1,234 @@
+# pygameを読み込む
 import pygame
+# sysライブラリを読み込む
 import sys
+# pygameのlocalsファイルにあるすべての変数、関数、クラスを読み込む
 from pygame.locals import *
+# GUI表示ライブラリtkinterを読み込む
 import tkinter as tk
+# os関連の処理をするosライブラリを読み込む
 import os
+# snake.pyにあるすべての変数、関数、クラスを読み込む
 from snake import *
+# food.pyにあるすべての変数、関数、クラスを読み込む
 from food import *
+# background.pyにあるすべての変数、関数、クラスを読み込む
 from background import *
+# effects.pyにあるすべての変数、関数、クラスを読み込む
 from effects import *
 
-
 class Game:
-    SIZE = WIDTH, HEIGHT = (800, 800)
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    fps = 20
-    score = 0
+  """
+  Gameクラス
 
-    def __init__(self):
-        os.environ['SDL_VIDEO_WINDOW_POS'] = f'{(tk.Tk().winfo_screenwidth() - self.WIDTH) // 2},' \
-                                             f'{(tk.Tk().winfo_screenheight() - self.HEIGHT) // 2}'
-        pygame.mixer.pre_init(44100, -16, 2, 2048)
-        pygame.mixer.init()
-        pygame.init()
-        pygame.mixer.music.load('System/Sounds/music.mid')
-        pygame.mixer.music.play(9)
-        pygame.display.set_caption('Snake')
+  Game全体を管理するクラス
+  """
+  # 画面の大きさ
+  SIZE = WIDTH, HEIGHT = (800, 800)
 
-        self.sound_game_over = pygame.mixer.Sound('System/Sounds/game_over.wav')
-        self.sound_eat_food = pygame.mixer.Sound('System/Sounds/eat_food.wav')
-        self.surf = pygame.display.set_mode(self.SIZE)
-        self.clock = pygame.time.Clock()
+  # 色情報をRGBで管理
+  WHITE = (255, 255, 255)
+  BLACK = (0, 0, 0)
+  RED = (255, 0, 0)
+  GREEN = (0, 255, 0)
 
-        self.bg = BG(self.surf)
-        self.effect = Effect(self.surf)
-        self.effect_trigger = False
-        self.snake = Snake(self.surf)
-        self.food = Food(self.surf)
+  # frame per second、1秒あたりのフレーム数。フレーム数が多いと滑らかになるが処理が遅くなる
+  fps = 20
+  # スコア
+  score = 0
 
-        self.font_score = pygame.font.Font(None, 22)
-        self.font_end = pygame.font.Font(None, 48)
-        self.font_button = pygame.font.Font(None, 30)
+  def __init__(self):
+    """
+    初期化関数
+    """
 
-        self.play()
+    # 謎の処理
+    os.environ["SDL_VIDEO_WINDOW_POS"] = f'{(tk.Tk().winfo_screenwidth() - self.WIDTH) // 2},' \
+      f'{(tk.Tk().winfo_screenheight() - self.HEIGHT) // 2}'
 
-    def play(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
+    # pygame mixerの初期設定
+    pygame.mixer.pre_init(44100, -16, 2, 2048)
+    pygame.mixer.init()
+    # pygameの初期化
+    pygame.init()
+    # MIDファイルのロード
+    pygame.mixer.music.load("System/Sounds/music.mid")
+    # 音楽を再生
+    pygame.mixer.music.play(9)
+    # ゲーム表示画面のキャプション名を設定
+    pygame.display.set_caption("Snake")
 
-            keys = pygame.key.get_pressed()
-            if keys[K_LEFT] or keys[K_a]:
-                if self.snake.direction != 'r':
-                    self.snake.direction = 'l'
-            elif keys[K_RIGHT] or keys[K_d]:
-                if self.snake.direction != 'l':
-                    self.snake.direction = 'r'
-            elif keys[K_UP] or keys[K_w]:
-                if self.snake.direction != 'd':
-                    self.snake.direction = 'u'
-            elif keys[K_DOWN] or keys[K_s]:
-                if self.snake.direction != 'u':
-                    self.snake.direction = 'd'
+    # 効果音「ゲームオーバー」を変数に設定
+    self.sound_game_over = pygame.mixer.Sound("System/Sounds/game_over.wav")
+    # 効果音「Snakeが食べ物を食べた時の音」を変数に設定
+    self.sound_eat_food = pygame.mixer.Sound("System/Sounds/eat_food.wav")
+    # 画像を描写するために使用するSurfaceクラスからインスタンスを生成
+    self.surf = pygame.display.set_mode(self.SIZE)
+    # 時間の設定
+    self.clock = pygame.time.Clock()
 
-            if self.food.is_eaten(self.snake.x, self.snake.y, self.snake.SIDE):
-                self.sound_eat_food.play()
-                self.snake.add_lenght()
-                self.effect_trigger = True
-                self.food.new_foodxy(self.snake.SIDE)
-                self.score += 1
-                self.fps += 1
+    # 背景のインスタンスを生成
+    self.bg = BG(self.surf)
+    # effectのインスタンスを生成
+    self.effect = Effect(self.surf)
+    # effectのトリガー変数
+    self.effect_trigger = False
+    # snakeのインスタンスを生成
+    self.snake = Snake(self.surf)
+    # foodのインスタンスを生成
+    self.food = Food(self.surf)
 
-            if (self.snake.x < 0 or self.snake.y < 0 or self.snake.x + self.snake.SIDE > self.WIDTH
-                    or self.snake.y + self.snake.SIDE > self.HEIGHT) or len(self.snake.XY) != len(set(self.snake.XY)):
-                self.game_over()
+    # スコア表示のフォント
+    self.font_score = pygame.font.Font(None, 22)
+    # 終了表示のフォント
+    self.font_end = pygame.font.Font(None, 48)
+    # ボタンのフォント
+    self.font_button = pygame.font.Font(None, 30)
 
-            self.draw()
+    # play関数を実行
+    self.play()
 
-    def draw(self):
-        self.surf.fill(self.BLACK)
-        self.surf.blit(self.font_score.render(f'SCORE: {self.score}', 1, (255, 165, 0)), (self.WIDTH - 90, 5))
+  def play(self):
+    """
+    キー操作
+    foodを食べた時の処理
+    ゲームオーバー判定
+    """
 
-        self.bg.draw(self.snake.direction)
-        self.food.add_food()
+    # ゲーム終了しない限り実行
+    while True:
+      # 終了コマンドを設定
+      for event in pygame.event.get():
+        if event.type == QUIT:
+          pygame.quit()
+          sys.exit()
 
-        self.snake.move_snake(self.snake.direction)
+      # キーを押したことを判定
+      keys = pygame.key.get_pressed()
 
-        self.effect.run(self.effect_trigger, self.snake.x, self.snake.y)
-        self.effect_trigger = False
+      # 左またはaを押した
+      if keys[K_LEFT] or keys[K_a]:
+        # 進行方向が右ではない
+        if self.snake.direction != "r":
+          # 進行方向を左に設定
+          self.snake.direction = "l"
+      # 右またはdを押した
+      elif keys[K_RIGHT] or keys[K_d]:
+        # 進行方向が左ではない
+        if self.snake.direction != "l":
+          # 進行方向を右に設定
+          self.snake.direction = "r"
+      # 上またはwを押した
+      elif keys[K_UP] or keys[K_w]:
+        # 進行方向が下ではない
+        if self.snake.direction != "d":
+          # 進行方向が上に設定
+          self.snake.direction = "u"
+      # 下またはsを押した
+      elif keys[K_DOWN] or keys[K_s]:
+        # 進行方向が上ではない
+        if self.snake.direction != "u":
+          # 進行方向が下に設定
+          self.snake.direction = "d"
 
-        pygame.display.update()
-        self.clock.tick(self.fps)
+      # foodがsnakeに食べられた
+      if self.food.is_eaten(self.snake.x, self.snake.y, self.snake.SIDE):
+        # foodを食べる効果音を再生
+        self.sound_eat_food.play()
+        # snakeの長さが伸びる
+        self.snake.add_lenght()
+        # effectのトリガーがTrue
+        self.effect_trigger = True
+        # 新しいfoodを配置
+        self.food.new_foodxy(self.snake.SIDE)
+        # スコアを加算
+        self.score += 1
+        # fpsが加算。速度が速くなり難易度が上がる
+        self.fps += 1
 
-    def game_over(self):
-        pygame.mixer.music.stop()
-        self.sound_game_over.play()
-        while True:
-            self.surf.blit(self.font_end.render(f'YOUR SCORE: {self.score}', 1, (255, 165, 0)),
-                           (self.WIDTH//2 - 130, self.HEIGHT//3))
+      # snakeが画面外に出たらゲームオーバー
+      if(self.snake.x < 0 or self.snake.y < 0 or self.snake.x + self.snake.SIDE > self.WIDTH or self.snake.y + self.snake.SIDE > self.HEIGHT) or len(self.snake.XY) != len(set(self.snake.XY)):
+        self.game_over()
 
-            mouse_coord = pygame.mouse.get_pos()
-            mouse_events = pygame.mouse.get_pressed()
-            if self.WIDTH//2 - 50 < mouse_coord[0] < self.WIDTH//2 + 50 and self.HEIGHT//2 - 50 < mouse_coord[1] < self.HEIGHT//2 - 10:
-                pygame.draw.rect(self.surf, self.GREEN, (self.WIDTH//2 - 50, self.HEIGHT//2-50, 100, 40))
-                if mouse_events[0]:
-                    break
-            else:
-                pygame.draw.rect(self.surf, self.RED, (self.WIDTH//2 - 50, self.HEIGHT//2 - 50, 100, 40))
-            self.surf.blit(self.font_button.render('RETRY', 1, self.BLACK),
-                           (self.WIDTH // 2 - 33, self.HEIGHT // 2 - 40))
-            self.clock.tick(self.fps)
-            pygame.display.update()
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-        Game()
+      # draw関数を実行
+      self.draw()
+
+  def draw(self):
+    """
+    描画関数
+    """
+    # 画面を黒に描画して初期化
+    self.surf.fill(self.BLACK)
+    # スコア表示
+    self.surf.blit(self.font_score.render(f'SCORE:{self.score}', 1 , (255, 165, 0)), (self.WIDTH - -90, 5))
+
+    # 背景を描画
+    self.bg.draw(self.snake.direction)
+    # foodを描画
+    self.food.add_food()
+    
+    # snakeの移動を描画
+    self.snake.move_snake(self.snake.direction)
+
+    # effectを実行
+    self.effect.run(self.effect_trigger, self.snake.x, self.snake.y)
+    # effectトリガーをFalse
+    self.effect_trigger = False
+
+    # pygameのディスプレイをupdate
+    pygame.display.update()
+    # fpsを更新
+    self.clock.tick(self.fps)
+
+  def game_over(self):
+    """
+    ゲームオーバー処理
+    """
+    # 音楽を停止
+    pygame.mixer.music.stop()
+    # 効果音「ゲームオーバー」を再生
+    self.sound_game_over.play()
+
+    # 次の処理をするまで実行し続ける
+    while True:
+      # スコアの結果を表示
+      self.surf.blit(self.font_end.render(f'YOUR SCORE:{self.score}', 1, (255, 165, 0)), (self.WIDTH // 2 -130, self.HEIGHT // 3))
+
+      # mouseのポインタ位置
+      mouse_coord = pygame.mouse.get_pos()
+      # mouseのクリックイベント
+      mouse_events = pygame.mouse.get_pressed()
 
 
-if __name__ == '__main__':
+      # RETRYボタンにマウスホバーすると色が変わる
+      if self.WIDTH // 2 - 50 < mouse_coord[0] < self.WIDTH // 2 + 50 and \
+        self.HEIGHT // 2 - 50 < mouse_coord[1] < self.HEIGHT // 2 -10:
+        # 緑の長方形を描画
+        pygame.draw.rect(self.surf, self.GREEN, (self.WIDTH // 2 - 50, self.HEIGHT // 2 - 50, 100,40))
+        if mouse_events[0]:
+          # RETRYボタン領域をクリック
+          break
+      else:
+        # 赤の長方形を描画
+        pygame.draw.rect(self.surf, self.RED, (self.WIDTH // 2 -50, self.HEIGHT // 2 - 50, 100, 40))
+
+      # RETRYの文字表示
+      self.surf.blit(self.font_button.render("RETRY", 1, self.BLACK), (self.WIDTH // 2 - 33, self.HEIGHT // 2 - 40))
+
+      # fpsを更新
+      self.clock.tick(self.fps)
+      # ディスプレイを更新
+      pygame.display.update()
+
+      # 終了コマンド
+      for event in pygame.event.get():
+        if event.type == QUIT:
+          pygame.quit()
+          sys.exit()
+
+    # Gameクラスを初期化してリトライ
     Game()
+
+if __name__ == "__main__":
+  # Gameクラスを初期化してゲームをスタート
+  Game()
